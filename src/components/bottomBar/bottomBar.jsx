0383d473fa269b5d3e0eb9c25092af616cfdb49b
geelona <mr.geelona@gmail.com>
2023-04-25 17:27:47 +0300

    filled the bottom bar and added scss styles

--- src/components/bottomBar/bottomBar.jsx
+++ src/components/bottomBar/bottomBar.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import GeelonaDescription from "./components/geelonaDescription";
 
 function BottomBar() {
   return (
     <div className="bottomBarContainer">
+      <GeelonaDescription />
     </div>
   );
 }
---
+++ src/components/bottomBar/components/geelonaDescription.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function GeelonaDescription() {
+  return (
+    <div className="geelonaDescriptionContainer">
+      <div><span>Made in China</span> by MrGeelona</div>
+    </div>
+  );
+}
+
+export default GeelonaDescription;
